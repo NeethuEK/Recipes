6532b23d0820eb25bf07b0989e2bbc2b0c402734
Neethu <neethuk109@gmail.com>
2022-05-17 20:54:33 -0400

    Initial Commit

---
+++ FetchRewards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchRewards
+//
+//  Created by Neethu Kuruvilla on 5/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
